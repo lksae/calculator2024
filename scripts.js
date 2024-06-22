6d8325daedcbbcdb1732f2177dad4b3ab5cbbd95
Matthias Schmitz <25723848+lksae@users.noreply.github.com>
2024-06-22 18:56:04 +0200

    basic calculator works

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
+let displayValue = "";
 
 
 function add (a,b) {
@@ -22,19 +23,69 @@ function divide (a,b) {
 function operate (operator, firstNumber, secondNumber) {
     switch (operator) {
         case 'add':
-            add(firstNumber, secondNumber);
+            return add(firstNumber, secondNumber);
+            break;
         case 'substract':
-            substract(firstNumber,secondNumber);
+            return substract(firstNumber,secondNumber);
+            break;
         case 'multiply':
-            multiply(firstNumber, secondNumber);
+            return multiply(firstNumber, secondNumber);
+            break;
         case 'divide':
-            divide(firstNumber, secondNumber);
+            return divide(firstNumber, secondNumber);
+            break;
     }
 }
+
+const display = document.querySelector(".display");
 
 const numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
-        console.log(button.innerHTML);
+        displayValue += button.innerHTML;
+        display.innerHTML = displayValue;
+
     })
 })
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        switch (button.getAttribute("id")) {
+            case "add":
+                operator = "add";
+                setFirstNumberAndDisplay();
+                break;
+            case "substract":
+                setFirstNumberAndDisplay();
+                operator = "substract";
+                break;
+            case "multiply":
+                setFirstNumberAndDisplay();
+                operator = "multiply";
+                break;
+            case "divide":
+                operator = "divide";
+                etFirstNumberAndDisplay();
+                
+                
+                break;
+            case "equal":
+                calculateAndDisplay()
+                break;
+        }
+    })
+})
+
+function setFirstNumberAndDisplay() {
+    firstNumber = parseFloat(displayValue);
+    display.innerHTML = 0;
+    displayValue = "";
+}
+
+function calculateAndDisplay(){
+    secondNumber = parseFloat(display.innerHTML);
+    displayValue = operate(operator, firstNumber,secondNumber);
+    display.innerHTML = displayValue;
+    firstNumber = 0;
+}
